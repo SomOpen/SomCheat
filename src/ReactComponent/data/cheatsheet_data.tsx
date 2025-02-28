d3e63ba1c2b2b98697c9dccd7acceb934a9db9f6
Adam-Elmi <Adamcade123@gmail.com>
2025-03-01 01:24:46 +0300

    Updated cheatsheet_data.tsx

--- src/ReactComponent/data/cheatsheet_data.tsx
+++ src/ReactComponent/data/cheatsheet_data.tsx
@@ -1,33 +1,61 @@
-import { JsIcon } from "../icons/Icon";
-const cheatsheet_data = [
-  {
-    name: "Javascript",
-    path: "cheatsheet/javascript",
-    icon: <JsIcon/>,
-    label: ["Web Development", "Language"],
-    updated: "Feb 27, 2025",
-  },
-  {
-    name: "C",
-    path: "cheatsheet/C",
-    icon: "",
-    label: ["Web Development", "Language"],
-    updated: "Feb 27, 2025",
-  },
-  {
-    name: "Golang",
-    path: "cheatsheet/golang",
-    icon: "",
-    label: ["Web Development", "Language"],
-    updated: "Feb 27, 2025",
-  },
-  {
-    name: "Lua",
-    path: "cheatsheet/lua",
-    icon: "",
-    label: ["Web Development", "Language"],
-    updated: "Feb 27, 2025",
-  },
-];
+import { FileCodeIcon, JsIcon } from "../icons/Icon";
+function dynamicPath(path: string) {
+  return `cheatsheets/${path.split(" ")[0].toLowerCase()}`;
+}
+
+const cheatsheet_data = {
+  // Programming Languages
+  "Programming Languages": [
+    {
+      name: "Javascript",
+      get path() {
+        return dynamicPath(this.name);
+      },
+      icon: <JsIcon dimension={32} />,
+      tags: ["Web Development", "Programming Language"],
+      updated: "Feb 27, 2025",
+    },
+    {
+      name: "C",
+      get path() {
+        return dynamicPath(this.name);
+      },
+      icon: "",
+      tags: ["General Purpose", "Programming Language"],
+      updated: "Feb 27, 2025",
+    },
+    {
+      name: "Golang",
+      get path() {
+        return dynamicPath(this.name);
+      },
+      icon: "",
+      tags: ["Backend", "Programming Language"],
+      updated: "Feb 27, 2025",
+    },
+    {
+      name: "Lua",
+      get path() {
+        return dynamicPath(this.name);
+      },
+      icon: "",
+      tags: ["Game Development", "Programming Language"],
+      updated: "Feb 27, 2025",
+    },
+  ],
+  // Databases
+  databases: [
+    {
+      name: "mongoDB",
+      get path() {
+        return dynamicPath(this.name);
+      },
+      icon: "",
+      tags: ["Backend", "Databases"],
+      updated: "Feb 27, 2025",
+    },
+  ],
+  //
+};
 
 export default cheatsheet_data;
