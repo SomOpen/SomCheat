009b5b4a10e03c81c3db19ce982088f937ed8e02
Adam-Elmi <Adamcade123@gmail.com>
2025-03-01 01:24:58 +0300

    Added utils directory

---
+++ src/utils/checklist.ts
@@ -0,0 +1,15 @@
+/*
+  Programming languages checklists
+  [ ] Variables & Data Types
+  [ ] Control Flow – If-else, switch, ternary operator
+  [ ] Loops & Iteration
+  [ ] Functions & Methods
+  [ ] Error Handling
+  [ ] Data Structures
+  [ ] Object-Oriented Basics – Classes, objects, inheritance, polymorphism
+  [ ] Modules & Imports
+  [ ] Asynchronous Programming
+  [ ] Memory Management
+  [ ] File Handling
+  [ ] Networking & APIs
+*/
