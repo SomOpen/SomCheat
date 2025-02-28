9d9f7af480d8aae874c16d40c61af271abb73f85
Adam-Elmi <Adamcade123@gmail.com>
2025-03-01 01:24:42 +0300

    Updated Cheatsheet.tsx

--- src/ReactComponent/components/Cheatsheet.tsx
+++ src/ReactComponent/components/Cheatsheet.tsx
@@ -1,17 +1,22 @@
-import cheatsheet_data from "../data/cheatsheet_data"
+import cheatsheet_data from "../data/cheatsheet_data";
+import { FileCodeIcon, DateIcon } from "../icons/Icon";
 
 export default function CheatSheet() {
-    return (
+  return (
+    <div>
+      {Object.entries(cheatsheet_data).map(([key, value]) => (
         <div>
-            {
-                cheatsheet_data ?
-                cheatsheet_data.map((cheatSheet, id) => (
-                    <div key={id}>
-                        {cheatSheet.icon}
-                    </div>
-                ))
-                 : null
-            }
+          <div>
+            <h1>{key}</h1>
+            {JSON.stringify(Object.values(cheatsheet_data))}
+          </div>
+          <div className="flex gap-2">
+            {value.map((cheatsheet, id) => (
+              <h1>{cheatsheet.name}</h1>
+            ))}
+          </div>
         </div>
-    )
-}  
+      ))}
+    </div>
+  );
+}
