c362b2f364786c2180ca6a469dc073cfdcafa0a3
Adam-Elmi <Adamcade123@gmail.com>
2025-03-01 08:21:23 +0300

    Updated Nav.tsx

--- src/ReactComponent/components/Nav.tsx
+++ src/ReactComponent/components/Nav.tsx
@@ -1,9 +1,11 @@
+import { useState, useRef, useEffect } from "react";
 import {
   HomeIcon,
   UpdateIcon,
   DictionaryIcon,
   CliIcon,
   AboutIcon,
+  MenuIcon,
 } from "../icons/Icon";
 
 interface Props {
@@ -41,16 +43,29 @@ const navData: Array<Props> = [
 ];
 
 export default function Nav() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function changeState() {
+    setIsVisible(prev => !prev);
+  }
+  console.log(isVisible)
   return (
-    <nav className="w-full border-b-[1.5px] border-slate-200 h-[50px]">
-      <ul className="flex gap-16 w-full justify-center items-center h-full flex-wrap">
+    <nav className="w-full border-b-[1.5px] border-slate-200 min-h-[50px] flex flex-col">
+      <button onClick={changeState} className="min-h-[50px] hidden max-sm:flex cursor-pointer w-full border-b-2 border-slate-100 p-2">
+        <MenuIcon dimension={35} />
+      </button>
+      <ul
+      // ref={listRef}
+        id="lists"
+        className={`flex ${isVisible ? "" : "max-sm:hidden"} max-sm:flex-col max-sm:items-start max-sm:gap-8 max-sm:p-l[1.4rem] gap-12 w-full justify-center items-center h-full flex-wrap p-2`}
+        >
         {navData &&
-          navData.map((section) => (
-            <li className="active-section flex gap-2 justify-center items-center h-full">
+          navData.map((section, id) => (
+            <li key={id} className="active-section flex gap-2 justify-center items-center h-full">
               {section.icon}
               <a
                 href={section.path}
-                className="font-medium text-slate-600 italic w-fit"
+                className="font-medium text-slate-600 hover:text-blue-400 italic w-fit"
               >
                 {section.sectionName}
               </a>
