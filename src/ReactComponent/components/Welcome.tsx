038b7e683679b9f8eb6c340f973905cf8687621a
Adam-Elmi <Adamcade123@gmail.com>
2025-03-01 08:21:29 +0300

    Updated Welcome.tsx

--- src/ReactComponent/components/Welcome.tsx
+++ src/ReactComponent/components/Welcome.tsx
@@ -1,34 +1,70 @@
 import { useState, useEffect } from "react";
-import { SmileIcon, SmileBeamIcon, WinkFaceIcon } from "../icons/Icon";
+import {
+  SmileIcon,
+  SmileBeamIcon,
+  WinkFaceIcon,
+  GithubIcon,
+  DocsIcon,
+} from "../icons/Icon";
+
+const welcome_messages: Array<object> = [
+  {
+    icon: <SmileIcon color="#6364f1" />,
+    message:
+      "Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam Hello Adam",
+  },
+  {
+    icon: <SmileBeamIcon color="#6364f1" />,
+    message: (
+      <>
+        <span>Welcome</span>{" "}
+        <span className="text-indigo-400">Adam</span>
+      </>
+    ),
+  },
+  {
+    icon: <WinkFaceIcon color="#6364f1" />,
+    message: "Enjoy coding",
+  },
+];
 
 export default function Welcome() {
-  const [state, setState] = useState<any>({});
+  const [state, setState] = useState<any>(welcome_messages[0]);
 
-  const welcome_messages: Array<object> = [
-    {
-      icon: <SmileIcon />,
-      message: "Hello Adam",
-    },
-    {
-      icon: <SmileBeamIcon />,
-      message: "Welcome Adam",
-    },
-    {
-      icon: <WinkFaceIcon />,
-      message: "Enjoy coding",
-    },
-  ];
   let counter = 0;
   useEffect(() => {
     let interval = setInterval(() => {
       counter++;
       if (counter === 3) counter = 0;
       setState(welcome_messages[counter]);
-    }, 2000);
+    }, 3000);
     return () => {
       clearInterval(interval);
     };
   }, []);
-  const { message } = state;
-  return <h1>{state ? state.icon : null}</h1>;
+  const { icon, message } = state;
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center">
+      {state ? (
+        <div className="flex gap-2 items-center justify-center flex-wrap">
+          <span>{icon}</span>
+          <p
+            className="italic font-bold text-center"
+          >
+            {message}
+          </p>
+        </div>
+      ) : null}
+      <div className="flex gap-3">
+        <button className="cursor-pointer flex gap-2 items-center border-2 border-slate-500 p-2 rounded-md bg-[#111122e5]">
+          <span className="font-semibold text-slate-200 hover:text-blue-300">Contribute</span>
+          <GithubIcon color="#e2e8f0"/>
+        </button>
+        <button className="flex gap-2 items-center cursor-pointer border-2 border-slate-300 p-2 rounded-md bg-slate-100">
+          <span className="font-semibold text-indigo-400 hover:text-blue-300">Read Docs</span>
+          <DocsIcon color="oklch(0.673 0.182 276.935)"/>
+        </button>
+      </div>
+    </div>
+  );
 }
