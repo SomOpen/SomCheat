6aa53126e93437545d263a2b594e3cda0f38ae54
Adam-Elmi <Adamcade123@gmail.com>
2025-03-01 08:21:30 +0300

    Updated cheatsheet_data.tsx

--- src/ReactComponent/data/cheatsheet_data.tsx
+++ src/ReactComponent/data/cheatsheet_data.tsx
@@ -1,61 +1,118 @@
-import { FileCodeIcon, JsIcon } from "../icons/Icon";
+import { JsIcon } from "../icons/Icon";
 function dynamicPath(path: string) {
   return `cheatsheets/${path.split(" ")[0].toLowerCase()}`;
 }
 
-const cheatsheet_data = {
-  // Programming Languages
-  "Programming Languages": [
-    {
-      name: "Javascript",
-      get path() {
-        return dynamicPath(this.name);
-      },
-      icon: <JsIcon dimension={32} />,
-      tags: ["Web Development", "Programming Language"],
-      updated: "Feb 27, 2025",
+// Programming Languages
+export const languagesData = [
+  {
+    name: "Javascript", 
+    get path() {
+      return dynamicPath(this.name);
     },
-    {
-      name: "C",
-      get path() {
-        return dynamicPath(this.name);
-      },
-      icon: "",
-      tags: ["General Purpose", "Programming Language"],
-      updated: "Feb 27, 2025",
+    icon: <JsIcon dimension={32} />,
+    tags: ["Web Development", "Programming Language"],
+    lastUpdate: "Feb 27, 2025",
+  },
+  {
+    name: "C",
+    get path() {
+      return dynamicPath(this.name);
     },
-    {
-      name: "Golang",
-      get path() {
-        return dynamicPath(this.name);
-      },
-      icon: "",
-      tags: ["Backend", "Programming Language"],
-      updated: "Feb 27, 2025",
+    icon: "",
+    tags: ["General Purpose", "Programming Language"],
+    lastUpdate: "Feb 27, 2025",
+  },
+  {
+    name: "Golang",
+    get path() {
+      return dynamicPath(this.name);
     },
-    {
-      name: "Lua",
-      get path() {
-        return dynamicPath(this.name);
-      },
-      icon: "",
-      tags: ["Game Development", "Programming Language"],
-      updated: "Feb 27, 2025",
+    icon: "",
+    tags: ["Backend", "Programming Language"],
+    lastUpdate: "Feb 27, 2025",
+  },
+  {
+    name: "Lua",
+    get path() {
+      return dynamicPath(this.name);
     },
-  ],
-  // Databases
-  databases: [
-    {
-      name: "mongoDB",
-      get path() {
-        return dynamicPath(this.name);
-      },
-      icon: "",
-      tags: ["Backend", "Databases"],
-      updated: "Feb 27, 2025",
+    icon: "",
+    tags: ["Game Development", "Programming Language"],
+    lastUpdate: "Feb 27, 2025",
+  },
+  {
+    name: "Lua",
+    get path() {
+      return dynamicPath(this.name);
     },
-  ],
-  //
-};
+    icon: "",
+    tags: ["Game Development", "Programming Language"],
+    lastUpdate: "Feb 27, 2025",
+  },
+];
 
-export default cheatsheet_data;
+// Databases
+export const databasesData = [
+  {
+    name: "mongoDB",
+    get path() {
+      return dynamicPath(this.name);
+    },
+    icon: "",
+    tags: ["Backend", "Databases"],
+    lastUpdate: "Feb 27, 2025",
+  },
+];
+
+// Web Development
+export const webDevelopmentData = [
+  {
+    name: "Css",
+    get path() {
+      return dynamicPath(this.name);
+    },
+    icon: "",
+    tags: ["Frontend", "Web Development"],
+    lastUpdate: "Feb 27, 2025",
+  },
+];
+
+// Operating System
+export const operatingSystemData = [
+  {
+    name: "Linux",
+    get path() {
+      return dynamicPath(this.name);
+    },
+    icon: "",
+    tags: ["Operating System"],
+    lastUpdate: "Feb 27, 2025",
+  },
+];
+
+// Operating System
+export const versionControlData = [
+  {
+    name: "Git",
+    get path() {
+      return dynamicPath(this.name);
+    },
+    icon: "",
+    tags: ["Version Control"],
+    lastUpdate: "Feb 27, 2025",
+  },
+];
+
+// Dev Tools
+export const devToolsData = [
+  {
+    name: "Git",
+    get path() {
+      return dynamicPath(this.name);
+    },
+    icon: "",
+    tags: ["Dev Tools"],
+    lastUpdate: "Feb 27, 2025",
+  },
+];
