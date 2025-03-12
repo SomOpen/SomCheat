fc5dc2e8331cf60cdd56ec6bbd348a81491ac436
Adam-Elmi <Adamcade123@gmail.com>
2025-03-12 15:37:45 +0300

    Update progress.ts

---
+++ src/utils/progress.ts
@@ -0,0 +1,11 @@
+export default function getProgress(getIndex: any, getData: any, obj: any) {
+  try {
+    obj.id = obj.id.split(" ")[0].toLowerCase();
+    const index = getIndex(obj.id);
+    return index !== null
+      ? parseInt(getData(index)[obj.id]?.finished_tasks_progress)
+      : 0;
+  } catch (error) {
+    console.error(error);
+  }
+}
