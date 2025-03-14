77e743399b06d613e25b3a9d4d4d3371a23d65e1
Adam-Elmi <Adamcade123@gmail.com>
2025-03-14 09:35:16 +0300

    Update Layout.tsx

--- src/ReactComponent/components/Layout.tsx
+++ src/ReactComponent/components/Layout.tsx
@@ -197,3 +197,27 @@ export const Link: React.FC<LinkProps> = ({ title, url, description }) => {
     </a>
   );
 };
+
+interface IntroProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const Intro: React.FC<IntroProps> = ({ title, children }) => {
+  return (
+    <div className="w-full p-5 border-2 border-gray-200 bg-blue-50">
+      {/* Top Bar with Label */}
+      <div className="flex justify-between items-center mb-3">
+        <span className="text-md text-white bg-blue-500 px-2 py-1 rounded">
+          Introduction
+        </span>
+      </div>
+
+      {/* Title */}
+      <h3 className="text-2xl font-bold text-slate-700 border-b-2 border-dashed w-fit mb-2 py-2">{title}</h3>
+
+      {/* Description */}
+      <p className="mt-2 text-sm text-gray-600 leading-relaxed">{children}</p>
+    </div>
+  );
+};
