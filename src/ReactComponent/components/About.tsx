9c7d3587313d8fbbbd10d49dd50d7160e1e7074a
Adam-Elmi <Adamcade123@gmail.com>
2025-03-22 14:53:51 +0300

    Update About component

--- src/ReactComponent/components/About.tsx
+++ src/ReactComponent/components/About.tsx
@@ -1,27 +1,39 @@
+import { WelcomeIcon } from "../icons/Welcome_Icons";
 const About = () => {
   return (
-    <main className="flex flex-col items-center justify-center p-8 bg-gray-50">
-      {/* Header */}
-      <h1 className="text-2xl sm:text-4xl font-bold text-gray-900 mb-6 text-center">
-        SomCheat - Xasuusin Degdeg Ah
-      </h1>
+    <main className="hero flex flex-col items-center justify-center p-8 bg-gray-50">
+      <div className="relative w-[900px] pt-16 max-w-full bg-white border-2 border-slate-200 min-h-[300px] shadow-md p-4 rounded-lg">
+        {/* Icon */}
+        <span className="absolute top-2 left-[50%] border-b-2 pb-2 border-slate-200">
+          {<WelcomeIcon dimension={34} color="#62748e" />}
+        </span>
+        {/* Header */}
+        <h1 className="text-2xl sm:text-4xl font-bold text-slate-600 mb-6 text-center">
+          SomCheat - Xasuusin Degdeg Ah
+        </h1>
 
-      {/* Description */}
-      <p className="text-lg sm:text-xl text-gray-700 text-center max-w-2xl mb-8 leading-relaxed">
-        <span className="font-bold text-indigo-400">SomCheat</span> waa mashruuc muhiim ah oo loogu talagalay in lagu xasuusto
-        waxyaabaha aasaasiga ah ee tiknoolajiyada ee ay developers u baahan
-        yihiin. Waxaa abuuray <span className="font-bold text-cyan-500">Adam Elmi</span> si uu u fududeeyo barashada iyo
-        horumarka developer-yada Soomaalida.
-      </p>
+        {/* Description */}
+        <p className="text-lg sm:text-xl text-slate-500 w-full mb-8 leading-relaxed italic">
+          <span className="font-bold text-indigo-400">SomCheat</span> waa
+          mashruuc loogu talagalay in lagu xasuusto waxyaabaha aasaasiga ah ee
+          tiknoolajiyada, sida luuqadaha programming-ka(habka code-ka lo qoro iyo maclumaadka muhiimka ah ee ka caawinaya inaad programming-ka fahanto). Waxa mashruucan sameeyey{" "}
+          <a href="https://github.com/Adam-Elmi" className="underline font-bold text-slate-500 hover:text-blue-300">Adam Elmi</a> si uu u
+          fududeeyo barashada iyo horumarka tiknoolajiyada.
+        </p>
 
-      {/* Buttons */}
-      <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-        <button className="px-6 py-3 bg-indigo-500 text-white rounded-lg hover:bg-indigo-700">
-          Daawo Cheat Sheets
-        </button>
-        <button className="px-6 py-3 border border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-600 hover:text-white">
-          Ku Biir Mashruuca
-        </button>
+        {/* Buttons */}
+        <div className="flex gap-4 justify-center items-center flex-wrap w-full">
+          <a href="/cheatsheets">
+            <button className="cursor-pointer shadow-md px-6 py-3 bg-indigo-500 text-white rounded-lg hover:bg-indigo-700">
+              View Cheat Sheets
+            </button>
+          </a>
+          <a href="https://github.com/Adam-Elmi/SomCheat">
+            <button className="cursor-pointer shadow-md px-6 py-3 border border-indigo-600 text-indigo-600 rounded-lg hover:bg-indigo-600 hover:text-white">
+              Contribute
+            </button>
+          </a>
+        </div>
       </div>
     </main>
   );
