6376e2656056e4ee6944152bb3a1fc2d7c19b2dc
Adam-Elmi <Adamcade123@gmail.com>
2025-03-23 18:44:51 +0300

    Updated icon_detector.tsx

--- src/ReactComponent/helpers/icon_detector.tsx
+++ src/ReactComponent/helpers/icon_detector.tsx
@@ -1,6 +1,23 @@
+import pathJson from "../../../data/path.json";
+
 import { FileCodeIcon, JsIcon } from "../icons/File_Icons"
 
-export const detector = {
+const all_file_icons = {
     default: <FileCodeIcon/>,
     js: <JsIcon/>
+};
+
+export default function icon_detector(filename: string) {
+    try {
+        if(pathJson && all_file_icons) {
+            for(const [key, value] of Object.entries(pathJson)) {
+                if(filename === key && all_file_icons.hasOwnProperty(key)) {
+                    return all_file_icons[key as keyof typeof all_file_icons];
+                }
+            }
+        }
+        return all_file_icons.default;
+    } catch (error) {
+        console.error(error);
+    }
 }
