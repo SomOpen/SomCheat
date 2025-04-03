e29c6ff254f1aea00efdb6ae04c4299230522ce5
Adam-Elmi <Adamcade123@gmail.com>
2025-04-03 14:37:53 +0300

    Updated src/ReactComponent/helpers/cheatsheet_data.tsx

--- src/ReactComponent/helpers/cheatsheet_data.tsx
+++ src/ReactComponent/helpers/cheatsheet_data.tsx
@@ -1,11 +1,15 @@
-import { FileCodeIcon, JsIcon } from "../icons/File_Icons";
+import { FileCodeIcon, JsIcon } from "../icons/lang_icons";
 import dynamicPath from "../../utils/dynamic_path";
 import { getData, getIndex } from "../../utils/handle_data";
 import getProgress from "../../utils/progress";
 import { filterTasks } from "../../utils/filter_tasks";
 import icon_detector from "./icon_detector";
 
-// Programming Languages
+/* 
+  ------------------
+  Programming languages
+  ------------------
+*/
 export const languagesData = [
   {
     id: "js",
@@ -22,8 +26,78 @@ export const languagesData = [
     },
   },
   {
+    id: "ts",
+    name: "Typescript",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
     id: "c",
     name: "C",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "java",
+    name: "Java",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "rust",
+    name: "Rust",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "swift",
+    name: "Swift",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "r",
+    name: "R",
     get path() {
       return dynamicPath(this.id);
     },
@@ -64,8 +138,92 @@ export const languagesData = [
     },
   },
   {
-    id: "lua",
-    name: "Lua",
+    id: "cs",
+    name: "C#",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "cpp",
+    name: "C++",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "py",
+    name: "Python",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "php",
+    name: "Php",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "rb",
+    name: "Ruby",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "kotlin",
+    name: "Kotlin",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "dart",
+    name: "Dart",
     get path() {
       return dynamicPath(this.id);
     },
@@ -78,9 +236,54 @@ export const languagesData = [
     },
   },
 ];
-
-// Databases
+/* 
+  ------------------
+  Databases
+  ------------------
+*/
 export const databasesData = [
+  {
+    id: "mysql",
+    name: "MySQL",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "postgresql",
+    name: "PostgreSQL",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "sqlite",
+    name: "SQLite",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
   {
     id: "mongodb",
     name: "MongoDB",
@@ -95,9 +298,41 @@ export const databasesData = [
       return getProgress(getIndex, getData, this);
     },
   },
+  {
+    id: "redis",
+    name: "Redis",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+  {
+    id: "firestore",
+    name: "Firestore",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
 ];
 
-// Web Development
+/* 
+  ------------------
+  Web development
+  ------------------
+*/
 export const webDevelopmentData = [
   {
     id: "css",
@@ -115,7 +350,11 @@ export const webDevelopmentData = [
   },
 ];
 
-// Operating System
+/* 
+  ------------------
+  Operating Systems
+  ------------------
+*/
 export const operatingSystemData = [
   {
     id: "linux",
@@ -151,7 +390,11 @@ export const versionControlData = [
   },
 ];
 
-// Dev Tools
+/* 
+  ------------------
+  Dev tools
+  ------------------
+*/
 export const devToolsData = [
   {
     id: "git",
