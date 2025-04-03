3d854cf29593038b8bac01a4098a70a478fd22cd
Adam-Elmi <Adamcade123@gmail.com>
2025-04-03 14:37:55 +0300

    Updated src/ReactComponent/helpers/icon_detector.tsx

--- src/ReactComponent/helpers/icon_detector.tsx
+++ src/ReactComponent/helpers/icon_detector.tsx
@@ -1,23 +1,17 @@
 import pathJson from "../../../data/path.json";
+import all_cheat_icons from "./list_icons";
 
-import { FileCodeIcon, JsIcon } from "../icons/File_Icons"
-
-const all_file_icons = {
-    default: <FileCodeIcon/>,
-    js: <JsIcon/>
-};
-
-export default function icon_detector(filename: string) {
-    try {
-        if(pathJson && all_file_icons) {
-            for(const [key, value] of Object.entries(pathJson)) {
-                if(filename === key && all_file_icons.hasOwnProperty(key)) {
-                    return all_file_icons[key as keyof typeof all_file_icons];
-                }
-            }
+export default function icon_detector(cheatname: string) {
+  try {
+    if (pathJson && all_cheat_icons) {
+      for (const [key, value] of Object.entries(pathJson)) {
+        if (cheatname === key && all_cheat_icons.hasOwnProperty(key)) {
+          return all_cheat_icons[key as keyof typeof all_cheat_icons];
         }
-        return all_file_icons.default;
-    } catch (error) {
-        console.error(error);
+      }
     }
+    return all_cheat_icons.default;
+  } catch (error) {
+    console.error(error);
+  }
 }
