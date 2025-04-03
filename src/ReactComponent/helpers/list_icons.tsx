25ea0224c3e149f64c74f20efd0deaf87c044110
Adam-Elmi <Adamcade123@gmail.com>
2025-04-03 14:38:10 +0300

    Added src/ReactComponent/helpers/list_icons.tsx

---
+++ src/ReactComponent/helpers/list_icons.tsx
@@ -0,0 +1,48 @@
+import { FirestoreIcon, MongoDBIcon, MySQLIcon, PostgreSQLIcon, RedisIcon, SQLLiteIcon } from "../icons/db_icons";
+import {
+    FileCodeIcon,
+    JsIcon,
+    TsIcon,
+    LuaIcon,
+    CIcon,
+    GoIcon,
+    CPPIcon,
+    CSharpIcon,
+    PyIcon,
+    PHPIcon,
+    RubyIcon,
+    KotlinIcon,
+    DartIcon,
+    JavaIcon,
+    RustIcon,
+    SwiftIcon,
+    RIcon
+  } from "../icons/lang_icons";
+  
+  const all_cheat_icons = {
+    default: <FileCodeIcon />,
+    js: <JsIcon />,
+    ts: <TsIcon/>,
+    lua: <LuaIcon />,
+    c: <CIcon />,
+    cpp: <CPPIcon />,
+    cs: <CSharpIcon />,
+    go: <GoIcon />,
+    py: <PyIcon />,
+    php: <PHPIcon />,
+    rb: <RubyIcon/>,
+    kotlin: <KotlinIcon/>,
+    dart: <DartIcon/>,
+    java: <JavaIcon/>,
+    rust: <RustIcon/>,
+    swift: <SwiftIcon/>,
+    r: <RIcon/>,
+    mysql: <MySQLIcon/>,
+    postgresql: <PostgreSQLIcon/>,
+    mongodb: <MongoDBIcon/>,
+    redis: <RedisIcon/>,
+    firestore: <FirestoreIcon/>,
+    sqlite: <SQLLiteIcon/>
+  };
+
+  export default all_cheat_icons;
