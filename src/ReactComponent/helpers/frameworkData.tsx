8654eb46400441acad2441cc341ced36a9c11b16
Adam-Elmi <Adamcade123@gmail.com>
2025-04-23 00:26:00 +0300

    Created framework data helper

---
+++ src/ReactComponent/helpers/frameworkData.tsx
@@ -0,0 +1,29 @@
+import dynamicPath from "../../utils/dynamic_path";
+import { getData, getIndex } from "../../utils/handle_data";
+import getProgress from "../../utils/progress";
+import icon_detector from "./icon_detector";
+
+/* 
+  ------------------
+  Frameworks
+  ------------------
+*/
+const frameworkData = [
+  {
+    id: "express.js",
+    name: "Express.js",
+    targets: "web",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+];
+
+export default frameworkData;
