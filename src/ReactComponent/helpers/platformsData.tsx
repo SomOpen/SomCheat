5d65941444d4ea67405d26846b2f148d7db3bed5
Adam-Elmi <Adamcade123@gmail.com>
2025-04-23 00:26:08 +0300

    Added platforms data helper

---
+++ src/ReactComponent/helpers/platformsData.tsx
@@ -0,0 +1,29 @@
+import dynamicPath from "../../utils/dynamic_path";
+import { getData, getIndex } from "../../utils/handle_data";
+import getProgress from "../../utils/progress";
+import icon_detector from "./icon_detector";
+
+/* 
+  ------------------
+  Platforms
+  ------------------
+*/
+const platformsData = [
+  {
+    id: "linux",
+    name: "Linux",
+    targets: "desktop",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+];
+
+export default platformsData;
