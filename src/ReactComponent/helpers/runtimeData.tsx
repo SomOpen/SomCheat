5eef843c9233bef173aef34143cce416840c6bc9
Adam-Elmi <Adamcade123@gmail.com>
2025-04-23 00:26:10 +0300

    Integrated runtime data helper

---
+++ src/ReactComponent/helpers/runtimeData.tsx
@@ -0,0 +1,28 @@
+import dynamicPath from "../../utils/dynamic_path";
+import { getData, getIndex } from "../../utils/handle_data";
+import getProgress from "../../utils/progress";
+import icon_detector from "./icon_detector";
+
+/* 
+  ------------------
+  Runtime
+  ------------------
+*/
+const runtimeData = [
+  {
+    id: "nodejs",
+    name: "Nodejs",
+    get path() {
+      return dynamicPath(this.id);
+    },
+    get icon() {
+      return icon_detector(this.id);
+    },
+    lastUpdate: "Feb 27, 2025",
+    get progress() {
+      return getProgress(getIndex, getData, this);
+    },
+  },
+];
+
+export default runtimeData;
