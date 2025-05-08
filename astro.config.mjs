9748fc40d983cd76970bc91b603c9f43c7af04b3
Adam-Elmi <Adamcade123@gmail.com>
2025-05-08 19:37:43 +0300

    Deploy to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/SomCheat/",
   markdown: {
     shikiConfig: {
-      theme: 'one-dark-pro',
-    }},
+      theme: "one-dark-pro",
+    },
+  },
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   server: {
-    host: true
+    host: true,
   },
 
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
 });
