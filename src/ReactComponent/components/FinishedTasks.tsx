da76836ec08d74f9115ccc7834632b2d90a305bf
Adam-Elmi <Adamcade123@gmail.com>
2025-05-25 21:43:04 +0300

    Updated FinishedTasks.tsx

--- src/ReactComponent/components/FinishedTasks.tsx
+++ src/ReactComponent/components/FinishedTasks.tsx
@@ -12,7 +12,7 @@ export default function FinishedTasks({
     <div
       id="finished-tasks-main"
       className="flex transition-all duration-300 linear
-                } flex-col border border-slate-300 rounded-md p-2"
+                } flex-col border border-slate-300 rounded-md p-2 dark:border-slate-800"
     >
       <button
         id="finished-tasks-btn"
