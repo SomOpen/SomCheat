e8e34bc025942227f2434ded1a15a93b8383828e
Adam-Elmi <Adamcade123@gmail.com>
2025-05-25 21:43:07 +0300

    Updated SearchBar.tsx

--- src/ReactComponent/components/SearchBar.tsx
+++ src/ReactComponent/components/SearchBar.tsx
@@ -43,20 +43,22 @@ export default function SearchBar() {
 
   return (
     <div className="relative flex justify-center items-center min-h-[50px] px-2 bg-transparent w-full">
-      <div className="bg-white w-[700px] max-w-full flex gap-4 justify-center items-center border border-slate-200 shadow-lg rounded-lg px-2 py-1 z-10">
+      <div className="bg-white dark:bg-[#111122] w-[700px] max-w-full flex gap-4 justify-center items-center dark:border-slate-700 border border-slate-200 shadow-lg rounded-lg px-2 py-1 z-10">
         <div className="flex gap-2 flex-1 justify-center items-center">
-          <SearchIcon />
+          <span className="px-2">
+            <SearchIcon />
+          </span>
           <input
             type="search"
             placeholder="Search"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             onKeyDown={handleKeyDown}
-            className="p-1 outline-none w-full"
+            className="p-1 outline-none w-full dark:text-slate-200"
           />
         </div>
         <button
-          className="p-2 bg-indigo-500 italic font-medium rounded-md text-slate-200 cursor-pointer border-2 border-slate-200"
+          className="p-2 bg-indigo-500 italic font-medium rounded-md text-slate-200 cursor-pointer dark:border-slate-700 border-2 border-slate-200"
           onClick={handleSearch}
         >
           Search
@@ -64,7 +66,7 @@ export default function SearchBar() {
       </div>
       <div className="px-2 mt-1 absolute w-full top-full flex justify-center items-center">
         {filteredResults.length > 0 && (
-          <div className=" bg-white border border-slate-300 shadow-lg rounded-lg w-full max-w-[700px] z-20">
+          <div className="dark:bg-[#111122] dark:border-slate-700 bg-white border border-slate-300 shadow-lg rounded-lg w-full max-w-[700px] z-20">
             <ul className="max-h-60 overflow-y-auto">
               {filteredResults.map((item, index) => (
                 <a
@@ -72,7 +74,7 @@ export default function SearchBar() {
                   href={item.path}
                   className="text-slate-400 italic"
                 >
-                  <li className="p-2 border-b border-slate-200 hover:bg-indigo-100 flex gap-2">
+                  <li className="p-2 dark:border-slate-700 dark:hover:bg-slate-900 border-b border-slate-200 hover:bg-indigo-100 flex gap-2">
                     <SheetIcon color="oklch(0.828 0.111 230.318)" />
                     {item.name}
                   </li>
